d32275ef090490c75e7a7271715b3531b7414520
jaquel <jaquelrey@gmail.com>
2022-10-20 03:15:07 -0400

    updated localstorage

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const fiveDay = document.getElementById("#fiveDayDisplay");
 const searchBtn = document.getElementById("#searchBtn");
 
 const searchHistory = [];
-const savedSearchHistory = [];
+
 
 let uviDisp = document.querySelector("#uvi");
 let uvDesc = document.querySelector("#uvDesc");
@@ -25,11 +25,11 @@ function getLatLon(userInput) {
     .then((data) => {
       console.log(
         userInput +
-          "'s found lat and lon is: lat: " +
-          data[0].lat +
-          " lon: " +
-          data[0].lon +
-          " using geocode.maps.co"
+        "'s found lat and lon is: lat: " +
+        data[0].lat +
+        " lon: " +
+        data[0].lon +
+        " using geocode.maps.co"
       );
 
       let foundLat = data[0].lat;
@@ -88,63 +88,64 @@ function fiveDayForecast(foundLat, foundLon) {
   const apiKey = "d3c11a611952a564411087138723b6e9";
   const api = `https://api.openweathermap.org/data/2.5/forecast?lat=${foundLat}&lon=${foundLon}&units=imperial&appid=${apiKey}`
   fetch(api)
-  .then((res) => {
-    return res.json();
-  })
-  .then((response) => {
+    .then((res) => {
+      return res.json();
+    })
+    .then((response) => {
 
-    console.log(response)
+      console.log(response)
 
-    const element = document.querySelectorAll(".forecastDay")
-    
-    for (i = 0; i < element.length; i++) {
-      element[i].innerHTML = "";
+      const element = document.querySelectorAll(".forecastDay")
 
-      const index = i * 8 + 4;
-      const daily = new Date(response.list[index].dt * 1000);
-      const day = daily.getDate();
-      const month = daily.getMonth() + 1;
-      const year = daily.getFullYear();
+      for (i = 0; i < element.length; i++) {
+        element[i].innerHTML = "";
 
-      const dateEl = document.createElement("p");
-      dateEl.setAttribute("class", "forecast-date");
-      dateEl.innerHTML = month + "/" + day + "/" + year;
-      element[i].append(dateEl);
+        const index = i * 8 + 4;
+        const daily = new Date(response.list[index].dt * 1000);
+        const day = daily.getDate();
+        const month = daily.getMonth() + 1;
+        const year = daily.getFullYear();
 
-      // Icon for current weather
-      const imageWrap = document.createElement("figure")
-      imageWrap.setAttribute("class", "image is-64x64")
-      element[i].append(imageWrap)
+        const dateEl = document.createElement("p");
+        dateEl.setAttribute("class", "forecast-date");
+        dateEl.innerHTML = month + "/" + day + "/" + year;
+        element[i].append(dateEl);
 
-      const imgUrl = "https://openweathermap.org/img/wn/" + response.list[index].weather[0].icon + ".png"
+        // Icon for current weather
+        const imageWrap = document.createElement("figure")
+        imageWrap.setAttribute("class", "image is-64x64")
+        element[i].append(imageWrap)
 
-      const imageEl = document.createElement("img")
-      imageEl.setAttribute("src", imgUrl);
-      imageEl.setAttribute("alt", response.list[index].weather[0].description);
+        const imgUrl = "https://openweathermap.org/img/wn/" + response.list[index].weather[0].icon + ".png"
 
-      imageWrap.append(imageEl)
+        const imageEl = document.createElement("img")
+        imageEl.setAttribute("src", imgUrl);
+        imageEl.setAttribute("alt", response.list[index].weather[0].description);
 
-      const tempEl = document.createElement("p");
-      tempEl.innerHTML = "Day temp: " + response.list[index].main.temp + "\u00B0 F";
-      element[i].append(tempEl);
+        imageWrap.append(imageEl)
 
-      // const nightEl = document.createElement("p");
-      // nightEl.innerHTML = "Night temp: " + response.list[index].main.humidity + "\u00B0 F";
-      // element[i].append(nightEl);
+        const tempEl = document.createElement("p");
+        tempEl.innerHTML = "Day temp: " + response.list[index].main.temp + "\u00B0 F";
+        element[i].append(tempEl);
 
-      const humidEl = document.createElement("p");
-      humidEl.innerHTML = "Humidity: " + response.list[index].main.humidity + "%";
-      element[i].append(humidEl);
+        // const nightEl = document.createElement("p");
+        // nightEl.innerHTML = "Night temp: " + response.list[index].main.humidity + "\u00B0 F";
+        // element[i].append(nightEl);
 
-      const windEl = document.createElement("p");
-      windEl.innerHTML = "Wind Speed: " + response.list[index].wind.speed + " MPH";
-      element[i].append(windEl);
+        const humidEl = document.createElement("p");
+        humidEl.innerHTML = "Humidity: " + response.list[index].main.humidity + "%";
+        element[i].append(humidEl);
 
-  }
-    
-  })
-  .catch((error) => console.log(error)
-  )}
+        const windEl = document.createElement("p");
+        windEl.innerHTML = "Wind Speed: " + response.list[index].wind.speed + " MPH";
+        element[i].append(windEl);
+
+      }
+
+    })
+    .catch((error) => console.log(error)
+    )
+}
 
 function uviDisplay(uvIndex) {
   if (uvIndex < 3) {
@@ -183,20 +184,24 @@ function queryEvent() {
 
   let cityInput = document.getElementsByName(userInput)[0];
 
-  searchHistory.push(userInput);
-  getLatLon(userInput);
+  if (!searchHistory.includes(cityInput)) {
+    searchHistory.push(userInput)
+    getLatLon(userInput)
 
-  if (cityInput != null) {
-    return;
-  } else if (!userInput) {
-    return;
-  } else {
-    makeBtn.classList.add("button", "is-fullwidth");
-    makeBtn.setAttribute("name", userInput);
-    makeBtn.textContent = userInput;
-    makeBtn.onclick = searchAgain;
-    cityHistory.append(makeBtn);
-    $(":input").val("");
+    localStorage.setItem("city", JSON.stringify(searchHistory))
+
+    if (cityInput != null) {
+      return
+    } else if (!userInput) {
+      return
+    } else {
+      makeBtn.classList.add("button", "is-fullwidth");
+      makeBtn.setAttribute("name", userInput);
+      makeBtn.textContent = userInput;
+      makeBtn.onclick = searchAgain;
+      cityHistory.append(makeBtn);
+      $(":input").val("");
+    }
   }
 }
 
@@ -206,6 +211,21 @@ function searchAgain() {
   $(":input").val("");
 }
 
-console.log(searchHistory);
+$(document).ready(function () {
+  let history = JSON.stringify(localStorage.getItem("city"))
+  
+
+  if (history != "null") {
+    const searchedHistory = history.split(',')
+    console.log(searchedHistory)
+    const index = searchedHistory.length - 1
+    const last = searchedHistory[index]
+    const lastSearch = last.replace(/\"|\[|\]|\\/g, '')
+
+    console.log(`Last searched city: ${lastSearch}`)
+    getLatLon(lastSearch)
+
+  }
+})
 
 // display searched data to window on click
